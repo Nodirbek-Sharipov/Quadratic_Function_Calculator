847d0c298201a75fd4111b9eba9c86ce73b73606
Nodirbek-Sharipov <33871499+Nodirbek-Sharipov@users.noreply.github.com>
2018-01-24 21:47:58 +0500

    Initial commit

---
+++ Quadratic_Function_Calculator/CalculatorToshi/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.numA = New System.Windows.Forms.NumericUpDown()
+        Me.numB = New System.Windows.Forms.NumericUpDown()
+        Me.numC = New System.Windows.Forms.NumericUpDown()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblDiscc = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.lblResX2 = New System.Windows.Forms.Label()
+        Me.lblResX1 = New System.Windows.Forms.Label()
+        Me.lblBottom2 = New System.Windows.Forms.Label()
+        Me.lblBottom = New System.Windows.Forms.Label()
+        Me.lblXtwoTOp = New System.Windows.Forms.LinkLabel()
+        Me.lblXoneTop = New System.Windows.Forms.LinkLabel()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.numA, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numB, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numC, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'numA
+        '
+        Me.numA.DecimalPlaces = 2
+        resources.ApplyResources(Me.numA, "numA")
+        Me.numA.Maximum = New Decimal(New Integer() {1569325055, 23283064, 0, 0})
+        Me.numA.Minimum = New Decimal(New Integer() {1874919423, 2328306, 0, -2147483648})
+        Me.numA.Name = "numA"
+        '
+        'numB
+        '
+        Me.numB.DecimalPlaces = 2
+        resources.ApplyResources(Me.numB, "numB")
+        Me.numB.Maximum = New Decimal(New Integer() {1569325055, 23283064, 0, 0})
+        Me.numB.Minimum = New Decimal(New Integer() {-1486618625, 232830643, 0, -2147483648})
+        Me.numB.Name = "numB"
+        '
+        'numC
+        '
+        Me.numC.DecimalPlaces = 2
+        resources.ApplyResources(Me.numC, "numC")
+        Me.numC.Maximum = New Decimal(New Integer() {1569325055, 23283064, 0, 0})
+        Me.numC.Minimum = New Decimal(New Integer() {-1486618625, 232830643, 0, -2147483648})
+        Me.numC.Name = "numC"
+        '
+        'btnCalculate
+        '
+        resources.ApplyResources(Me.btnCalculate, "btnCalculate")
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblDiscc)
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.lblResX2)
+        Me.GroupBox1.Controls.Add(Me.lblResX1)
+        Me.GroupBox1.Controls.Add(Me.lblBottom2)
+        Me.GroupBox1.Controls.Add(Me.lblBottom)
+        Me.GroupBox1.Controls.Add(Me.lblXtwoTOp)
+        Me.GroupBox1.Controls.Add(Me.lblXoneTop)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label14)
+        Me.GroupBox1.Controls.Add(Me.Label11)
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.TabStop = False
+        '
+        'lblDiscc
+        '
+        resources.ApplyResources(Me.lblDiscc, "lblDiscc")
+        Me.lblDiscc.Name = "lblDiscc"
+        '
+        'Label12
+        '
+        resources.ApplyResources(Me.Label12, "Label12")
+        Me.Label12.Name = "Label12"
+        '
+        'lblResX2
+        '
+        resources.ApplyResources(Me.lblResX2, "lblResX2")
+        Me.lblResX2.Name = "lblResX2"
+        '
+        'lblResX1
+        '
+        resources.ApplyResources(Me.lblResX1, "lblResX1")
+        Me.lblResX1.Name = "lblResX1"
+        '
+        'lblBottom2
+        '
+        resources.ApplyResources(Me.lblBottom2, "lblBottom2")
+        Me.lblBottom2.Name = "lblBottom2"
+        '
+        'lblBottom
+        '
+        resources.ApplyResources(Me.lblBottom, "lblBottom")
+        Me.lblBottom.Name = "lblBottom"
+        '
+        'lblXtwoTOp
+        '
+        resources.ApplyResources(Me.lblXtwoTOp, "lblXtwoTOp")
+        Me.lblXtwoTOp.LinkColor = System.Drawing.Color.Black
+        Me.lblXtwoTOp.Name = "lblXtwoTOp"
+        Me.lblXtwoTOp.TabStop = True
+        Me.lblXtwoTOp.VisitedLinkColor = System.Drawing.Color.Black
+        '
+        'lblXoneTop
+        '
+        resources.ApplyResources(Me.lblXoneTop, "lblXoneTop")
+        Me.lblXoneTop.LinkColor = System.Drawing.Color.Black
+        Me.lblXoneTop.Name = "lblXoneTop"
+        Me.lblXoneTop.TabStop = True
+        Me.lblXoneTop.VisitedLinkColor = System.Drawing.Color.Black
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'Label14
+        '
+        resources.ApplyResources(Me.Label14, "Label14")
+        Me.Label14.Name = "Label14"
+        '
+        'Label11
+        '
+        resources.ApplyResources(Me.Label11, "Label11")
+        Me.Label11.Name = "Label11"
+        '
+        'Label13
+        '
+        resources.ApplyResources(Me.Label13, "Label13")
+        Me.Label13.Name = "Label13"
+        '
+        'Label10
+        '
+        resources.ApplyResources(Me.Label10, "Label10")
+        Me.Label10.Name = "Label10"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.numC)
+        Me.Controls.Add(Me.numB)
+        Me.Controls.Add(Me.numA)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        CType(Me.numA, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numB, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numC, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents numA As NumericUpDown
+    Friend WithEvents numB As NumericUpDown
+    Friend WithEvents numC As NumericUpDown
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblXoneTop As LinkLabel
+    Friend WithEvents lblBottom2 As Label
+    Friend WithEvents lblBottom As Label
+    Friend WithEvents lblXtwoTOp As LinkLabel
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents lblResX2 As Label
+    Friend WithEvents lblResX1 As Label
+    Friend WithEvents lblDiscc As Label
+    Friend WithEvents Label12 As Label
+End Class
---
+++ Quadratic_Function_Calculator/CalculatorToshi/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim ainp As Double = numA.Text
+        Dim binp As Double = numB.Text
+        Dim cinp As Double = numC.Text
+
+        Dim disc As Double = binp ^ 2 - 4 * ainp * cinp
+
+        If disc >= 0 Then
+            Dim x1 As Double = Math.Round(((-1 * binp) - Math.Sqrt(disc)) / (2 * ainp), 2)
+            Dim x2 As Double = Math.Round(((-1 * binp) + Math.Sqrt(disc)) / (2 * ainp), 2)
+
+            lblXoneTop.Text = ((-1 * binp) - Math.Round(Math.Sqrt(disc), 4))
+            lblXtwoTOp.Text = ((-1 * binp) + Math.Round(Math.Sqrt(disc), 4))
+
+            lblBottom.Text = 2 * ainp
+            lblBottom2.Text = 2 * ainp
+
+            lblResX1.Text = x1
+            lblResX2.Text = x2
+
+            lblDiscc.Text = disc
+
+            MessageBox.Show("Discriminant: " & disc & vbNewLine & "X1 = " & x1 & vbNewLine & "X2 = " & x2, "Result", MessageBoxButtons.OK)
+
+        Else
+            lblXoneTop.Text = ""
+            lblXtwoTOp.Text = ""
+
+            lblBottom.Text = ""
+            lblBottom2.Text = ""
+
+            lblResX1.Text = ""
+            lblResX2.Text = ""
+
+            lblDiscc.Text = ""
+            MessageBox.Show("Discriminant is negative!" + vbNewLine + "The function does not cross the x-axis!", "Error", MessageBoxButtons.OK)
+        End If
+
+
+
+
+    End Sub
+
+
+End Class
